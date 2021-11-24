46bd5409e06bd2a56567e359952ab472e3024664
Nicolas <kolyakotik.vbg@gmail.com>
2021-11-24 22:45:57 +0300

    again commit

--- trains/urls.py
+++ trains/urls.py
@@ -3,7 +3,8 @@ from django.urls import path
 from trains.views import *
 
 urlpatterns = [
-    path('', TrainListView.as_view(), name='home'),
+    path('', home, name='home'),
+    # path('', TrainListView.as_view(), name='home'),
     # path('detail/<int:pk>/', TrainDetailView.as_view(), name='detail'),
     # path('update/<int:pk>/', TrainUpdateView.as_view(), name='update'),
     # path('delete/<int:pk>/', TrainDeleteView.as_view(), name='delete'),
--- trains/views.py
+++ trains/views.py
@@ -10,24 +10,25 @@ from django.views.generic import DetailView, CreateView, UpdateView, DeleteView,
 from trains.models import Train
 
 __all__ = (
-    'home', 'TrainListView',
-    #'TrainDetailView', 'TrainCreateView', 'TrainUpdateView', 'TrainDeleteView',
+    'home',
+    # 'TrainDetailView', 'TrainCreateView', 'TrainUpdateView', 'TrainDeleteView', 'TrainListView',
 )
 
 
 def home(request, pk=None):
-    qs = Train.objects.all()
+    # qs = Train.objects.all()
+    qs = Train.objects.select_related('from_city.station', 'from_city.platform',
+                                      'to_city.station', 'to_city.platform')
     lst = Paginator(qs, 5)
     page_number = request.GET.get('page')
     page_obj = lst.get_page(page_number)
     context = {'page_obj': page_obj}
     return render(request, 'trains/home.html', context)
 
-
-class TrainListView(ListView):
-    paginate_by = 5
-    model = Train
-    template_name = 'trains/home.html'
+# class TrainListView(ListView):
+#     paginate_by = 5
+#     model = Train
+#     template_name = 'trains/home.html'
 
 
 # class TrainDetailView(DetailView):
@@ -61,4 +62,3 @@ class TrainListView(ListView):
 #         return self.post(request, *args, **kwargs)
 #
 #
-
